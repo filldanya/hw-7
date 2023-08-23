d10f046c975dafec2bfc22e35958040255aaa996
Daniil <daniilfilichew@gmail.com>
2023-08-23 19:03:48 +0300

    second commit

--- hw 7-1/hw 7-1/main.cpp
+++ hw 7-1/hw 7-1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-#define MODE 1
+#define MODE 0
+#if MODE == 1
 	int add() {
 		int a;
 		int b;
@@ -12,15 +13,19 @@
 		return a + b;
 
 	}
+#endif
 int main() {
 	setlocale(LC_ALL, "RUS");
 #if !defined(MODE)
-#error
+#error "Необходимо определить MODE"
+	
 #elif MODE == 0
 	std::cout << "Работаю в режиме тренировки";
 #elif MODE == 1
 	std::cout << "Работаю в боевом режиме" << std::endl;
 
 	add();
+#elif MODE != 0 && MODE != 1
+	std::cout << "Неизвестный режим. Завершение работы";
 #endif
 }
